244a03da7b5b6b55447fc7695bdc850116ed0197
Gunshui <minqinyang@163.com>
2015-05-28 16:12:19 +0800

    11
    fdf

--- gitHub/OneViewController.m
+++ gitHub/OneViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     UIButton*btn=[[UIButton alloc]init];
+    NSLog(@"888");
 }
 
 - (void)didReceiveMemoryWarning {
